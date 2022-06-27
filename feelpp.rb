a4c830f939ab8f7e16bde3bed471ed5d70fed217
Christophe Prud'homme <christophe.prudhomme@cemosis.fr>
2022-06-27 23:47:30 +0200

    up homebrew for feelpp

--- feelpp.rb
+++ feelpp.rb
@@ -13,16 +13,12 @@ class GitNoSubmoduleDownloadStrategy < GitDownloadStrategy
 end
 
 class Feelpp < Formula
-  homepage 'http://www.feelpp.org'
+  homepage 'http://docs.feelpp.org'
   url 'https://github.com/feelpp/feelpp/releases/download/v0.104.0-beta.1/feelpp-0.104.0-beta.1.tar.gz'
   head 'https://github.com/feelpp/feelpp.git', :using => GitNoSubmoduleDownloadStrategy, :branch => 'develop'
-  version '0.104.0-beta.1'
+  version '0.110.0-rc.2'
   sha256 '70418fb0ce9f5069190fcc1677615663dbca71cea39e2b858356476a9e8627cf'
 
-
-  depends_on 'autoconf' => :build
-  depends_on 'automake' => :build
-  depends_on 'libtool' => :build
   depends_on 'cmake' => :build
   depends_on 'cln'
   depends_on 'mongo-c'
@@ -32,68 +28,21 @@ class Feelpp < Formula
   depends_on 'scalapack'
   depends_on 'petsc'
   depends_on 'slepc' => :recommended
-  depends_on 'boost' => ['c++11']
-  depends_on 'boost-mpi' => ['c++11']
-  depends_on 'boost-python' => ['c++11']
+  depends_on 'boost' => ['c++17']
+  depends_on 'boost-mpi' => ['c++17']
   depends_on 'ann' => :recommended
   depends_on 'glpk' => :recommended
   depends_on 'doxygen' => :optional
 
   def install
-    # had to keep application for mesh_partitioner
-    args=std_cmake_args+ ['-DFEELPP_ENABLE_TESTS=OFF', '-DFEELPP_ENABLE_IPOPT=OFF', '-Wno-dev']
-
-    dirs = ['cmake', 'contrib', 'feel', 'applications/mesh', 'quickstart']
-    Dir.mkdir 'opt'
-    cd 'opt' do
-      system "cmake", "..", *args
-      dirs.each do |dir|
-        cd "#{dir}" do
-          system "make", "-j#{ENV.make_jobs}"
-          system "make", "install"
-        end
-      end
-    end
-
-    dirs = ['quickstart']
-    cd 'opt' do
-      dirs.each do |dir|
-        cd "#{dir}" do
-          system "make", "-j#{ENV.make_jobs}"
-          system "make", "install"
-          system "ctest", "-R", "."
-        end
-      end
-    end
+    system "cmake","--preset","feelpp"
+    system "cmake","--build","--preset","feelpp"
+    system "cmake","--build","--preset","feelpp","install"
   end
 
   # create a CMakeLists.txt and a test.cpp initializing the environment
   # and try to compile it
   test do
-    (testpath/"CMakeLists.txt").write <<-EOS.undent
-      if ( ${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_CURRENT_SOURCE_DIR} )
-        find_package(Feel++
-                PATHS $ENV{FEELPP_DIR}/share/feelpp/feel/cmake/modules
-                      /usr/share/feelpp/feel/cmake/modules
-                      /usr/local/share/feelpp/feel/cmake/modules
-                      /opt/share/feelpp/feel/cmake/modules
-                      )
-        if(NOT FEELPP_FOUND)
-          message(FATAL_ERROR "Feel++ was not found on your system. Make sure to install it and specify the FEELPP_DIR to reference the installation directory.")
-        endif()
-      endif()
-      feelpp_add_application(test_homebrew SRCS test.cpp )
-    EOS
-    (testpath/"test.cpp").write <<-EOS.undent
-      #include <feel/feelcore/environment.hpp>
-      using namespace Feel;
-      int main(int argc, char** argv)
-      {
-        Environment env( _argc=argc, _argv=argv);
-        return 0;
-      }
-    EOS
-    system "cmake", "."
-    system 'make', 'feelpp_test_homebrew'
+#    system 'ctest', '--preset','feelpp','qs'
   end
 end
