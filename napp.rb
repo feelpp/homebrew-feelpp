cfa08f51935deff77682d037e7cb091a558d5162
Christophe Prud'homme <christophe.prudhomme@cemosis.fr>
2024-12-16 00:20:18 +0100

    up archive

--- napp.rb
+++ napp.rb
@@ -1,8 +1,8 @@
 class Napp < Formula
     desc "NAPP: C++ implementation of named arguments for functions"
     homepage "https://github.com/vincentchabannes/napp" # Replace with the official homepage URL
-    url "https://github.com/vincentchabannes/napp/archive/v1.0.0.tar.gz" # Replace with actual tarball URL
-    sha256 "examplechecksum1234567890abcdef1234567890abcdef1234567890abcdef" # Replace with actual SHA256 checksum
+    url "https://github.com/vincentchabannes/napp/archive/refs/tags/v0.3.0.tar.gz" # Replace with actual tarball URL
+#    sha256 "examplechecksum1234567890abcdef1234567890abcdef1234567890abcdef" # Replace with actual SHA256 checksum
     license "MIT" # Replace with the appropriate license
   
     depends_on "cmake" => :build
